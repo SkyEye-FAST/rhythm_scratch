b404022938f7fcd58c7d61e9d0930df28d21a6ba
SkyEye_FAST <skyeyefast@teahou.se>
2023-08-14 03:03:20 +0800

    Optimize program structure

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## 曲库
 
-目前支持的音游：Arcaea、Phigros、Orzmic
+目前支持的音游：Arcaea、Phigros、Orzmic。
 
 - 曲库使用的Arcaea版本：v4.5.3
 - 曲库使用的Phiros版本：3.1.1.1
@@ -10,58 +10,11 @@
 
 ## 命令列表
 
-<table>
-<thead align="center">
-  <tr>
-    <th>命令</th>
-    <th>功能</th>
-  </tr>
-</thead>
-<tbody align="center">
-  <tr>
-    <td colspan="2">通用命令</td>
-  </tr>
-  <tr>
-    <td>h | help | ?</td>
-    <td>显示帮助</td>
-  </tr>
-  <tr>
-    <td>e | exit</td>
-    <td>退出</td>
-  </tr>
-  <tr>
-    <td>v | ver | version</td>
-    <td>显示曲库使用的音游版本号</td>
-  </tr>
-  <tr>
-    <td colspan="2">一次性命令</td>
-  </tr>
-  <tr>
-    <td>(u | use) &lt;num&gt;</td>
-    <td>选择使用的曲库</td>
-  </tr>
-  <tr>
-    <td colspan="2">游戏中可用命令</td>
-  </tr>
-  <tr>
-    <td>s | show</td>
-    <td>显示题目</td>
-  </tr>
-  <tr>
-    <td>(o | open) &lt;char&gt;</td>
-    <td>刮开某字符</td>
-  </tr>
-  <tr>
-    <td>(c | correct) &lt;num&gt;</td>
-    <td>将某题全部刮开</td>
-  </tr>
-  <tr>
-    <td>(d | decrease) &lt;num&gt;</td>
-    <td>减少可用刮开次数</td>
-  </tr>
-  <tr>
-    <td>(i | increase) &lt;num&gt;</td>
-    <td>增加可用刮开次数</td>
-  </tr>
-</tbody>
-</table>
+- help | ? - 显示帮助
+- exit - 退出
+- version | ver | v - 列出曲库使用音游版本号
+- (heart | h) add [amount] - 增加可用刮开次数
+- (heart | h) remove [amount] - 减少可用刮开次数
+- (open | o) [character] - 刮开指定字符
+- (check | c) [index] - 将某题全部刮开
+- (show | s) - 显示题目
--- scratch.py
+++ scratch.py
@@ -1,39 +1,52 @@
 # -*- encoding: utf-8 -*-
 
-import re, os, sys, pyperclip
+import re, os, sys, pyperclip, tomllib
 from random import sample
 
-# 调用路径
-p = (
-    os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.path.sep + ".")
-    + os.path.sep
-)
+# 路径
+P = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.path.sep + ".")
+DICT_FOLDER = os.path.join(P, "dict")
+OUTPUT_FOLDER = os.path.join(P, "output")
 
 # 创建输出文件夹（若不存在）
-if not os.path.exists(p + "output"):
-    os.makedirs(p + "output")
+if not os.path.exists(OUTPUT_FOLDER):
+    os.makedirs(OUTPUT_FOLDER)
 
-NUM = 10  # 生成曲目数量
-ARCVER = "v4.5.3"
-PGRVER = "3.1.1.1"
-ORZVER = "2.18.21"
+# 加载配置
+with open("configuration.toml", "rb") as f:
+    config = tomllib.load(f)
+
+NUM = config["generate_amount"]  # 生成曲目数量
+GUESS_CHANCES = config["guess_chances"]  # 最初刮开可用次数
+
+# 加载曲库列表
+games = []
+versions = []
+all_dicts_folder = []
+for i in os.listdir(DICT_FOLDER):
+    dict_config = os.path.join(DICT_FOLDER, i, "dict.toml")
+    if os.path.exists(dict_config):
+        all_dicts_folder.append(i)
+        with open(dict_config, "rb") as f:
+            toml = tomllib.load(f)
+            games.append(toml["name"])
+            versions.append(toml["version"])
 
 
-# 定义初始化曲库函数
-def ini(name, dict):
-    f = open(p + "dict" + os.path.sep + name, "r", encoding="utf-8")
-    line = f.readline()
-    while line:
-        dict.append(line[:-1])
-        line = f.readline()
-    f.close()
+# 定义将曲库加载为列表
+def load_dict(file_path):
+    dict_content = []
+    with open(os.path.join(DICT_FOLDER, file_path), "r", encoding="utf-8") as file:
+        for line in file:
+            word = line.strip()
+            dict_content.append(word)
+    return dict_content
 
 
 # 定义复制文件内容到剪贴板函数
 def copy(file):
-    f = open(p + "output" + os.path.sep + file, "r", encoding="utf-8")
-    content = f.read()
-    pyperclip.copy(content)
+    with open(os.path.join(OUTPUT_FOLDER, file), "r", encoding="utf-8") as f:
+        pyperclip.copy(f.read())
 
 
 # 定义已开字符函数
@@ -50,255 +63,215 @@ def known_char(name):
 
 # 定义输出文件函数
 def output_file(name, file):
-    o = open(p + "output" + os.path.sep + file, "w", encoding="utf-8")
-    i = 0
-    for e in name:
-        i += 1
-        o.write(str(i) + ". " + e + "\n")
-    o.close()
+    with open(os.path.join(OUTPUT_FOLDER, file), "w", encoding="utf-8") as f:
+        for i, element in enumerate(name):
+            f.write(f"{i + 1}. {element}\n")
 
 
 # 定义输出暂存到文件函数
 def output_temp(known, name, file):
-    o = open(p + "output" + os.path.sep + file, "w", encoding="utf-8")
-    if known != "":
-        o.write(known + "\n")
-    i = 0
-    for e in name:
-        i += 1
-        o.write(str(i) + ". " + e + "\n")
-    o.close()
+    with open(os.path.join(OUTPUT_FOLDER, file), "w", encoding="utf-8") as f:
+        if known != "":
+            f.write(known + "\n")
+        for i, element in enumerate(name):
+            f.write(f"{i + 1}. {element}\n")
 
 
 # 定义输出函数
 def output_print(name):
-    i = 0
-    for e in name:
-        i += 1
-        print(str(i) + ". " + e)
+    for i, element in enumerate(name):
+        print(f"{i + 1}. {element}")
 
 
 # 定义显示帮助
 def print_help():
-    print(
-        """+----------------------+--------------------------+
-|         命令         |           功能           |
-+----------------------+--------------------------+
-|                     通用命令                    |
-+----------------------+--------------------------+
-|     h | help | ?     |         显示帮助         |
-+----------------------+--------------------------+
-|       e | exit       |           退出           |
-+----------------------+--------------------------+
-|   v | ver | version  | 显示曲库使用的音游版本号 |
-+----------------------+--------------------------+
-|                    一次性命令                   |
-+----------------------+--------------------------+
-|    (u | use) <num>   |      选择使用的曲库      |
-+----------------------+--------------------------+
-|                  游戏中可用命令                 |
-+----------------------+--------------------------+
-|       s | show       |         显示题目         |
-+----------------------+--------------------------+
-|   (o | open) <char>  |        刮开某字符        |
-+----------------------+--------------------------+
-|  (c | correct) <num> |      将某题全部刮开      |
-+----------------------+--------------------------+
-| (d | decrease) <num> |     减少可用刮开次数     |
-+----------------------+--------------------------+
-| (i | increase) <num> |     增加可用刮开次数     |
-+----------------------+--------------------------+"""
-    )
+    print("可用命令：")
+    print("  help | ? - 显示帮助")
+    print("  exit - 退出")
+    print("  version | ver | v - 列出曲库使用音游版本号")
+    print("  (heart | h) add [amount] - 增加可用刮开次数")
+    print("  (heart | h) remove [amount] - 减少可用刮开次数")
+    print("  (open | o) [character] - 刮开指定字符")
+    print("  (check | c) [index] - 将某题全部刮开")
+    print("  (show | s) - 显示题目")
 
 
-# 定义通用命令
-def uni_cmds(r):
-    if re.match(r"^(version|ver|v)$", r):
-        print(
-            "曲库使用的Arcaea版本：%s\n曲库使用的Phiros版本：%s\n曲库使用的Orzmic版本：%s"
-            % (ARCVER, PGRVER, ORZVER)
-        )
-    elif re.match(r"^(exit|e)$", r):
+def main():
+    print("音游猜曲名刮刮乐\n作者：SkyEye_FAST\n\n可用的曲库：")
+    i = 0
+    while i < len(games):
+        print(f"{i + 1}. {games[i]}（游戏版本：{versions[i]}）")
+        i += 1
+
+    selected_dicts = input("请选择曲库编号，以逗号分隔：\n\n>> ").split(",")
+    selected_dicts = [
+        int(index.strip()) for index in selected_dicts if index.strip().isdigit()
+    ]
+
+    if len(selected_dicts) == 0:
+        print("未选择任何曲库。")
         sys.exit()
-    elif re.match(r"^(help|h|\?)$", r):
-        print_help()
-    else:
-        print("无效的命令。")
 
-
-# 定义命令相关函数
-def cmd_char_judge(c, s):
-    if re.match("^" + c + "\\s", s):
-        return True
-    else:
-        return False
-
-
-def cmd_num_judge(c, s):
-    if re.match("^" + c + "\\s", s):
-        return True
-    else:
-        return False
-
-
-def cmd_num_len(c, s):
-    return len(s) - len(c) - 1
-
-
-print(
-    """
-音游猜曲名刮刮乐
-作者：SkyEye_FAST
-
-目前支持的音游：Arcaea、Phigros、Orzmic
-曲库使用的Arcaea版本：%s
-曲库使用的Phiros版本：%s
-曲库使用的Orzmic版本：%s
-"""
-    % (ARCVER, PGRVER, ORZVER)
-)
-print_help()
-
-# 选择游戏，初始化曲库
-d = []
-while d == []:
-    r = input(
-        "\n请使用命令“use”或“u”选择曲库（可多选，如“u 12”）\n1. Arcaea\n2. Phigros\n3. Orzmic\n\n>> "
-    )
-    r.replace("use", "u")
-    if cmd_num_judge("u", r):
-        n = r[2:]
-        if "1" in n:
-            ini("arc_slst", d)
-            ini("arc_other", d)
-        if "2" in n:
-            ini("pgr", d)
-        if "3" in n:
-            ini("orz", d)
-        d = list(set(d))  # 去除重复曲目
-        print("选择曲目总数：%d\n" % len(d))
-    else:
-        uni_cmds(r)
-
-# 生成答案
-a = sample(d, NUM)
-output_file(a, "Answer")
-
-# 生成初始问题
-q = []
-for e in a:
-    e = re.sub(r"\S", "*", e)
-    q.append(e)
-output_file(q, "Question")
-copy("Question")
-print("题目：")
-output_print(q)
-
-# 刮卡
-r = ""  # 输入的内容
-t = q  # 题目暂存
-t1 = q  # 题目暂存的暂存
-char = []  # 刮开的字符，拉丁字母大小写
-char1 = []  # 刮开的字符，拉丁字母全部小写
-heart = 20  # 刮开可用次数
-
-while heart > 0 and t != a:
-    r = input("\n>> ")  # 输入命令
-
-    # 替换别名
-    r = r.replace("open", "o")
-    r = r.replace("correct", "c")
-    r = r.replace("decrease", "d")
-    r = r.replace("increase", "i")
-
-    if cmd_char_judge("o", r):
-        if len(r) != 3:
-            print("无效的参数，应为单个字符。")
+    selected_dict_content = []
+    for index in selected_dicts:
+        if index > 0 and index <= len(all_dicts_folder):
+            dict_config = os.path.join(
+                DICT_FOLDER, all_dicts_folder[index - 1], "dict.toml"
+            )
+            with open(dict_config, "rb") as f:
+                toml = tomllib.load(f)
+                for element in toml["dicts"]:
+                    selected_dict_content += load_dict(
+                        os.path.join(
+                            DICT_FOLDER, all_dicts_folder[index - 1], "dict", element
+                        )
+                    )
+            print(f"已加载曲库“{games[index - 1]}”。")
         else:
-            input_char = r[-1]
-            if input_char in "-$( )*+.[]{{}}?\\^|/":
-                input_char = "\\" + input_char  # 转义字符
+            print(f"编号为“{index}”的曲库不存在，忽略。")
 
-            if input_char in char:
-                print("这个字符已经刮开了！剩余次数：%d。" % heart)
+    selected_dict_content = list(set(selected_dict_content))  # 去除重复曲目
+    print(f"选择曲目总数：{len(selected_dict_content)}\n")
+
+    # 生成答案
+    answer_list = sample(selected_dict_content, 10)
+    output_file(answer_list, "Answer")
+
+    # 生成初始问题
+    question_list = []
+    for element in answer_list:
+        question_list.append("*" * len(element))
+    output_file(question_list, "Question")
+    copy("Question")
+    print("题目：")
+    output_print(question_list)
+
+    # 刮卡
+    heart = GUESS_CHANCES  # 刮开可用次数
+    t = question_list  # 题目暂存
+    t1 = question_list  # 题目暂存的暂存
+    char = []  # 刮开的字符，拉丁字母大小写
+    opened_char = []  # 刮开的字符，拉丁字母全部小写
+
+    while heart > 0 and t != answer_list:
+        command = input("\n>> ")  # 输入命令
+
+        # 替换命令别名
+        command_aliases = {
+            "heart": "h",
+            "open": "o",
+            "check": "c",
+            "exit": "e",
+            "show": "s",
+            "ver": "v",
+            "version": "v",
+            "help": "?",
+            "？": "?",
+        }
+        parts = command.split()
+        action = parts[0]
+        if action in command_aliases:
+            action = command_aliases[action]
+
+        if action == "?":
+            print_help()
+        elif action == "e":
+            sys.exit()
+        elif action == "v":
+            i = 0
+            while i < len(games):
+                print(f"曲库使用的{games[i]}版本：{versions[i]}\n")
+                i += 1
+        elif action == "o":
+            if len(parts[1]) != 1:
+                print("无效的参数，应为单个字符。")
             else:
-                char1.append(str.lower(r[-1]))
-                if re.match(r"^[A-Za-z]$", input_char):  # 判断是否为拉丁字母
-                    char.append(str.lower(input_char))  # 加入小写
-                    char.append(str.upper(input_char))  # 加入大写
+                input_char = parts[1]
+                if input_char in "-$( )*+.[]{{}}?\\^|/":
+                    input_char = "\\" + input_char  # 转义字符
+
+                if input_char in char:
+                    print(f"这个字符已经刮开了！剩余次数：{heart}。")
                 else:
-                    char.append(input_char)  # 加入非拉丁字母字符
-                heart -= 1
-                print("刮开的字符：%s。\n剩余次数：%d。" % (r[-1], heart))
-                # 为正则替换而合并char为字符串
-                used_char = ""
-                for i in range(len(char)):
-                    used_char += char[i]
-                # 使用正则替换刮开字符
-                t = []
-                for e in a:
-                    e = re.sub("[^" + used_char + "\\s]", "*", e)
-                    t.append(e)
-                # 将回答正确的全部刮开
-                for i in range(NUM):
-                    if t1[i] == a[i]:
-                        t[i] = a[i]
-                output_temp(known_char(char1), t, "Temp")
-                copy("Temp")
-                if known_char(char1) != "":
-                    print(known_char(char1))
-                output_print(t)
-                t1 = t  # 暂存
-                t = []  # 清空
+                    opened_char.append(str.lower(parts[1]))
+                    if re.match(r"^[A-Za-z]$", input_char):  # 判断是否为拉丁字母
+                        char.append(str.lower(input_char))  # 加入小写
+                        char.append(str.upper(input_char))  # 加入大写
+                    else:
+                        char.append(input_char)  # 加入非拉丁字母字符
+                    heart -= 1  # 扣除1点可用刮开次数
+                    print(f"刮开的字符：{parts[1]}。\n剩余次数：{heart}。")
+                    # 为正则替换而合并char为字符串
+                    used_char = ""
+                    for i in range(len(char)):
+                        used_char += char[i]
+                    # 使用正则替换刮开字符
+                    t = []
+                    for element in answer_list:
+                        t.append(re.sub("[^" + used_char + "\\s]", "*", element))
+                    # 将回答正确的全部刮开
+                    for i in range(NUM):
+                        if t1[i] == answer_list[i]:
+                            t[i] = answer_list[i]
+                    output_temp(known_char(opened_char), t, "Temp")
+                    copy("Temp")
+                    if known_char(opened_char) != "":
+                        print(known_char(opened_char))
+                    output_print(t)
+                    t1 = t  # 暂存
+                    t = []  # 清空
 
-    elif cmd_num_judge("c", r):
-        if not str.isdigit(r[2:]):
-            print("无效的参数，应为数字。")
-        else:
-            n = int(r[2:])
-            if n > NUM or n < 1:
-                print("题目不存在。")
-            elif t1[n - 1] != a[n - 1]:
-                print("%d.回答正确，全部刮开。" % n)
-                t1[n - 1] = a[n - 1]
-                output_temp(known_char(char1), t1, "Temp")
-                copy("Temp")
-                if known_char(char1) != "":
-                    print(known_char(char1))
-                output_print(t1)
-                t = t1
+        elif action == "c":
+            if not str.isdigit(parts[1]):
+                print("无效的参数，应为数字。")
             else:
-                print("%d.已回答正确。" % n)
+                n = int(parts[1])
+                if n > NUM or n < 1:
+                    print("题目不存在。")
+                elif t1[n - 1] != answer_list[n - 1]:
+                    print("编号为“%d”的题目回答正确，全部刮开。" % n)
+                    t1[n - 1] = answer_list[n - 1]
+                    output_temp(known_char(opened_char), t1, "Temp")
+                    copy("Temp")
+                    if known_char(opened_char) != "":
+                        print(known_char(opened_char))
+                    output_print(t1)
+                    t = t1
+                else:
+                    print("编号为“%d”的题目已经回答正确。" % n)
 
-    elif cmd_num_judge("d", r):
-        if not str.isdigit(r[2:]):
-            print("无效的参数，应为数字。")
+        elif action == "h":
+            if len(parts) >= 2:
+                if parts[1] == "add":
+                    amount = int(parts[2]) if len(parts) > 2 else 1
+                    heart += amount
+                    print(f"已增加{amount}次可用刮开次数。\n剩余次数：{heart}。")
+                elif parts[1] == "remove":
+                    amount = int(parts[2]) if len(parts) > 2 else 1
+                    heart -= amount
+                    print(f"已减少{amount}次可用刮开次数。\n剩余次数：{heart}。")
+                else:
+                    print("无效的命令，请重试。")
+            else:
+                print("无效的命令，请重试。")
+
+        elif action == "s":
+            if t == []:
+                output_print(t1)
+            else:
+                output_print(t)
+            copy("Temp")
         else:
-            n = int(r[2:])
-            heart -= n
-            print("已减少%d次可用刮开次数。\n剩余次数：%d。" % (n, heart))
+            print("无效的命令，请重试。")
 
-    elif cmd_num_judge("i", r):
-        if not str.isdigit(r[2:]):
-            print("无效的参数，应为数字。")
+    if action != "e":
+        if t == answer_list:
+            print("\n回答正确，答案为：")
         else:
-            n = int(r[2:])
-            heart += n
-            print("已增加%d次可用刮开次数。\n剩余次数：%d。" % (n, heart))
+            print("\n次数已耗尽，答案为：")
+        output_print(answer_list)
+        copy("Answer")
 
-    elif re.match(r"^(show|s)$", r):
-        if t == []:
-            output_print(t1)
-        else:
-            output_print(t)
-        copy("Temp")
-    else:
-        uni_cmds(r)
 
-if not re.match(r"^(exit|e)$", r):
-    if t == a:
-        print("\n回答正确，答案为：")
-    else:
-        print("\n次数已耗尽，答案为：")
-    output_print(a)
-    copy("Answer")
+if __name__ == "__main__":
+    main()
