40789e559ef11cf9f555b2edafb9738d509191ab
SkyEye_FAST <skyeyefast@teahou.se>
2023-08-14 23:48:56 +0800

    Optimize

--- scratch.py
+++ scratch.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 
-import re, os, sys, pyperclip, tomllib
+import os, re, sys, pyperclip, tomllib
 from random import sample
 
 # 加载配置
@@ -17,28 +17,24 @@ DICT_FOLDER = os.path.join(P, config["path"]["dict_folder"])  # 曲库路径
 OUTPUT_FOLDER = os.path.join(P, config["path"]["output_folder"])  # 输出路径
 
 # 创建输出文件夹（若不存在）
-if not os.path.exists(OUTPUT_FOLDER):
-    os.makedirs(OUTPUT_FOLDER)
+os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
 # 加载曲库列表
-games, versions, all_dicts_folder = [], [], []
+games = versions = all_dicts_folder = []
 for i in os.listdir(DICT_FOLDER):
-    dict_config = os.path.join(DICT_FOLDER, i, "dict.toml")
-    if os.path.exists(dict_config):
+    dict_config_file = os.path.join(DICT_FOLDER, i, "dict.toml")
+    if os.path.exists(dict_config_file) and (
+        dict_config := tomllib.load(open(dict_config_file, "rb"))
+    ):
         all_dicts_folder.append(i)
-        with open(dict_config, "rb") as f:
-            toml = tomllib.load(f)
-            games.append(toml["name"])
-            versions.append(toml["version"])
+        games.append(dict_config["name"])
+        versions.append(dict_config["version"])
 
 
 # 定义加载曲库为列表函数
 def load_dict(file_path):
-    dict_content = []
     with open(os.path.join(DICT_FOLDER, file_path), "r", encoding="utf-8") as file:
-        for line in file:
-            word = line.strip()
-            dict_content.append(word)
+        dict_content = [line.strip() for line in file]
     return dict_content
 
 
@@ -50,11 +46,8 @@ def copy(file):
 
 # 定义已开字符函数
 def known_char(name):
-    if name != []:
-        s = "已开字符：" + name[0]
-        for i in range(1, len(name)):
-            s += "、" + name[i]
-        return s + "。"
+    if name:
+        return "已开字符：" + "、".join(name) + "。"
     else:
         return ""
 
@@ -63,16 +56,15 @@ class output:
     # 定义输出文件函数
     def to_file(name, file):
         with open(os.path.join(OUTPUT_FOLDER, file), "w", encoding="utf-8") as f:
-            for i, element in enumerate(name):
-                f.write(f"{i + 1}. {element}\n")
+            f.writelines(f"{i + 1}. {element}\n" for i, element in enumerate(name))
 
     # 定义输出暂存到文件函数
     def to_temp(known, name, file):
         with open(os.path.join(OUTPUT_FOLDER, file), "w", encoding="utf-8") as f:
-            if known != "":
+            if known:
                 f.write(known + "\n")
-            for i, element in enumerate(name):
-                f.write(f"{i + 1}. {element}\n")
+            lines = [f"{i + 1}. {element}\n" for i, element in enumerate(name)]
+            f.writelines(lines)
 
     # 定义循环输出函数
     def loop_print(name):
@@ -95,10 +87,8 @@ class output:
 def main():
     print("音游猜曲名刮刮乐\n作者：SkyEye_FAST\n\n可用的曲库：")
     # 输出曲库列表
-    i = 0
-    while i < len(games):
-        print(f"{i + 1}. {games[i]}（游戏版本：{versions[i]}）")
-        i += 1
+    for i, (game, version) in enumerate(zip(games, versions), start=1):
+        print(f"{i}. {game}（游戏版本：{version}）")
     # 选择曲库
     selected_dicts = input("请选择曲库编号，以逗号分隔：\n\n>> ").split(",")
     selected_dicts = [
@@ -108,23 +98,25 @@ def main():
         print("未选择任何曲库。")
         sys.exit()
     # 加载曲库
-    selected_dict_content = []
-    for index in selected_dicts:
-        if index > 0 and index <= len(all_dicts_folder):
-            dict_config = os.path.join(
-                DICT_FOLDER, all_dicts_folder[index - 1], "dict.toml"
+    selected_dict_content = [
+        load_dict(
+            os.path.join(DICT_FOLDER, all_dicts_folder[index - 1], "dict", element)
+        )
+        for index in selected_dicts
+        if 0 < index <= len(all_dicts_folder)
+        for element in tomllib.load(
+            open(
+                os.path.join(DICT_FOLDER, all_dicts_folder[index - 1], "dict.toml"),
+                "rb",
             )
-            with open(dict_config, "rb") as f:
-                toml = tomllib.load(f)
-                for element in toml["dicts"]:
-                    selected_dict_content += load_dict(
-                        os.path.join(
-                            DICT_FOLDER, all_dicts_folder[index - 1], "dict", element
-                        )
-                    )
+        )["dicts"]
+    ]
+    for index in selected_dicts:
+        if 0 < index <= len(all_dicts_folder):
             print(f"已加载曲库“{games[index - 1]}”。")
         else:
             print(f"编号为“{index}”的曲库不存在，忽略。")
+
     selected_dict_content = list(set(selected_dict_content))  # 去除重复曲目
     print(f"选择曲目总数：{len(selected_dict_content)}\n")
 
@@ -143,10 +135,8 @@ def main():
 
     # 刮卡
     heart = GUESS_CHANCES  # 刮开可用次数
-    t = question_list  # 题目暂存
-    tt = question_list  # 题目暂存的暂存
-    opened_char = []  # 刮开的字符，拉丁字母大小写
-    opened_char_lowercase = []  # 刮开的字符，拉丁字母全部小写
+    t = tt = question_list  # 题目暂存、题目暂存的暂存
+    opened_char = opened_char_lowercase = []  # 刮开的字符（拉丁字母大小写、拉丁字母全部小写）
 
     while heart > 0 and t != answer_list:
         command = input("\n>> ")  # 输入命令
@@ -188,67 +178,67 @@ def main():
                 if input_char in opened_char:
                     print(f"这个字符已经刮开了！剩余次数：{heart}。")
                 else:
-                    opened_char_lowercase.append(str.lower(parts[1]))
+                    opened_char_lowercase.append(input_char.lower())
                     if re.match(r"^[A-Za-z]$", input_char):  # 判断是否为拉丁字母
-                        opened_char.append(str.lower(input_char))  # 加入小写
-                        opened_char.append(str.upper(input_char))  # 加入大写
+                        opened_char.extend(
+                            [input_char.lower(), input_char.upper()]
+                        )  # 加入小写和大写
                     else:
                         opened_char.append(input_char)  # 加入非拉丁字母字符
                     heart -= 1  # 扣除1点可用刮开次数
                     print(f"刮开的字符：{parts[1]}。\n剩余次数：{heart}。")
-                    # 为正则替换而合并opened_char为字符串
-                    used_char = ""
-                    for i in range(len(opened_char)):
-                        used_char += opened_char[i]
-                    # 使用正则替换刮开字符
-                    t = []
-                    for element in answer_list:
-                        t.append(re.sub("[^" + used_char + "\\s]", "*", element))
-                    # 将回答正确的全部刮开
-                    for i in range(NUM):
-                        if tt[i] == answer_list[i]:
-                            t[i] = answer_list[i]
+                    used_char = "".join(opened_char)  # 合并opened_char为字符串
+                    t = [
+                        re.sub("[^" + used_char + "\\s]", "*", element)
+                        for element in answer_list
+                    ]  # 使用正则替换刮开字符
+                    t = [
+                        answer_list[i] if tt[i] == answer_list[i] else t[i]
+                        for i in range(NUM)
+                    ]  # 将回答正确的全部刮开
                     output.to_temp(known_char(opened_char_lowercase), t, "Temp")
                     copy("Temp")
-                    if known_char(opened_char_lowercase) != "":
-                        print(known_char(opened_char_lowercase))
+                    known = known_char(opened_char_lowercase)
+                    if known:
+                        print(known)
                     output.loop_print(t)
-                    tt = t  # 暂存
-                    t = []  # 清空
+                    tt, t = t, []  # 更新暂存和清空
 
         elif action == "c":
-            if not str.isdigit(parts[1]):
+            if not parts[1].isdigit():
                 print("无效的参数，应为数字。")
             else:
                 n = int(parts[1])
                 if n > NUM or n < 1:
                     print("题目不存在。")
                 elif tt[n - 1] != answer_list[n - 1]:
-                    print("编号为“%d”的题目回答正确，全部刮开。" % n)
+                    print(f"编号为“{n}”的题目回答正确，全部刮开。")
                     tt[n - 1] = answer_list[n - 1]
                     output.to_temp(known_char(opened_char_lowercase), tt, "Temp")
                     copy("Temp")
-                    if known_char(opened_char_lowercase) != "":
-                        print(known_char(opened_char_lowercase))
+                    known = known_char(opened_char_lowercase)
+                    if known:
+                        print(known)
                     output.loop_print(tt)
                     t = tt
                 else:
-                    print("编号为“%d”的题目已经回答正确。" % n)
+                    print(f"编号为“{n}”的题目已经回答正确。")
 
         elif action == "h":
-            if len(parts) >= 2:
-                if parts[1] == "add":
+            if len(parts) == 3:
+                if parts[1] == "add" or parts[1] == "remove":
                     amount = int(parts[2]) if len(parts) > 2 else 1
-                    heart += amount
-                    print(f"已增加{amount}次可用刮开次数。\n剩余次数：{heart}。")
-                elif parts[1] == "remove":
-                    amount = int(parts[2]) if len(parts) > 2 else 1
-                    heart -= amount
-                    print(f"已减少{amount}次可用刮开次数。\n剩余次数：{heart}。")
+                    if parts[1] == "add":
+                        heart += amount
+                        print(f"已增加{amount}次可用刮开次数。\n剩余次数：{heart}。")
+                    else:
+                        heart -= amount
+                        print(f"已减少{amount}次可用刮开次数。\n剩余次数：{heart}。")
                 else:
                     print("无效的命令，请重试。")
             else:
                 print("无效的命令，请重试。")
+
 
         elif action == "s":
             if t == []:
