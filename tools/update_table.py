6404dce2b7903af2791d3cbd48f94f455a3fd4a9
SkyEye_FAST <skyeyefast@teahou.se>
2024-04-04 14:11:03 +0800

    Update Orzmic

--- tools/update_table.py
+++ tools/update_table.py
@@ -14,18 +14,19 @@ def get_data(url: str, song_row: int, folder: str, file: str):
     tables = soup.find_all("table")
 
     file_path = P / "song_dict" / folder / "dict" / file
-    with open(file_path, "w", encoding="utf-8") as file:
+    with open(file_path, "w", encoding="utf-8") as dict_file:
         for table in tables:
             rows = table.find_all("tr")
             for row in rows:
                 cells = row.find_all("td")
                 if len(cells) > 1:
                     data = cells[song_row].text
-                    file.write(data + "\n")
+                    dict_file.write(data + "\n")
 
 
 get_data("https://phigros.fandom.com/wiki/Songs", 0, "phigros", "pgr")
 get_data("https://musedash.fandom.com/wiki/Songs", 1, "muse_dash", "muse_dash")
+get_data("https://orzmic.fandom.com/wiki/Songs", 0, "orzmic", "orz")
 
 
 def fix_dict(folder: str, file: str):
@@ -35,10 +36,12 @@ def fix_dict(folder: str, file: str):
     with open(file_path, "r", encoding="utf-8") as dict_file:
         dict_content = sorted({line.strip() for line in dict_file})
 
-    while "" in dict_content:
-        dict_content.remove("")
+    dict_content = list(filter(None, dict_content))
     if folder == "muse_dash":
         dict_content.remove("HARD")
+    if folder == "orzmic":
+        dict_content.remove("-")
+        dict_content = list(filter(lambda x: not str(x).isdigit(), dict_content))
 
     with open(file_path, "w", encoding="utf-8") as dict_file:
         dict_file.writelines(f"{element}\n" for element in dict_content)
@@ -46,3 +49,4 @@ def fix_dict(folder: str, file: str):
 
 fix_dict("phigros", "pgr")
 fix_dict("muse_dash", "muse_dash")
+fix_dict("orzmic", "orz")
